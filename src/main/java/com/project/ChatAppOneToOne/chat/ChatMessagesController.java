a55eec3fc08d577c10d994b4853808f96fdf982e
Harshit <harshitjindal02@gmail.com>
2024-02-26 23:52:14 +0530

    completed the back-end

--- src/main/java/com/project/ChatAppOneToOne/chat/ChatMessagesController.java
+++ src/main/java/com/project/ChatAppOneToOne/chat/ChatMessagesController.java
@@ -3,6 +3,9 @@ package com.project.ChatAppOneToOne.chat;
 import com.project.ChatAppOneToOne.chatroom.ChatRoomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +16,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ChatMessagesController {
 
+    private final SimpMessagingTemplate messagingTemplate;
     private final ChatMessagesService chatMessagesService;
 
     @GetMapping("/messages/{senderId}/{recipientId}")
@@ -21,5 +25,15 @@ public class ChatMessagesController {
         return ResponseEntity.ok(chatMessagesService.findChatMessages(senderId, recipientId));
     }
 
-    
+    @MessageMapping("/chat")
+    public void processChatMessages(@Payload ChatMessages chatMessages) {
+        ChatMessages savedMessages = chatMessagesService.save(chatMessages);
+        messagingTemplate.convertAndSendToUser(chatMessages.getRecipientId(), "/queue/messages",
+                ChatNotification.builder()
+                        .id(savedMessages.getChatId())
+                        .senderId(chatMessages.getSenderId())
+                        .recipientId(chatMessages.getRecipientId())
+                        .content(chatMessages.getContent())
+                        .build());
+    }
 }
